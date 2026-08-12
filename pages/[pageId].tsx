f7f691f68be273e710675e71e415f50618dad022
otteru <damdori3@gmail.com>
2026-08-12 19:39:24 +0900

    fix: 빌드 시 전체 게시글 사전 생성 제거

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -1,8 +1,7 @@
 import { type GetStaticProps } from 'next'
 
 import { NotionPage } from '@/components/NotionPage'
-import { domain, isDev } from '@/lib/config'
-import { getSiteMap } from '@/lib/get-site-map'
+import { domain } from '@/lib/config'
 import { resolveNotionPage } from '@/lib/resolve-notion-page'
 import { type PageProps, type Params } from '@/lib/types'
 
@@ -25,29 +24,10 @@ export const getStaticProps: GetStaticProps<PageProps, Params> = async (
 }
 
 export async function getStaticPaths() {
-  if (isDev) {
-    return {
-      paths: [],
-      fallback: true
-    }
+  return {
+    paths: [],
+    fallback: 'blocking'
   }
-
-  const siteMap = await getSiteMap()
-
-  const staticPaths = {
-    paths: Object.keys(siteMap.canonicalPageMap)
-      .filter((pageId) => !pageId.includes('/'))
-      .map((pageId) => ({
-        params: {
-          pageId
-        }
-      })),
-    // paths: [],
-    fallback: true
-  }
-
-  console.log(staticPaths.paths)
-  return staticPaths
 }
 
 export default function NotionDomainDynamicPage(props: PageProps) {
